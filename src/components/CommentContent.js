2e91ca83e2a7ac054ff53a6378bd67421c901ff6
ninypops <ninypopsgame@gmail.com>
2022-02-14 10:12:25 +0000

    WIP Fetch comments from endpoint and use postId param to show for each post

--- src/components/CommentContent.js
+++ src/components/CommentContent.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const CommentContent = () => {
+const CommentContent = ({ postId }) => {
+    const [ comments, setPostComments ] = useState([]);
 
+    useEffect(() => {
+        loadPostComments();
+    }, []);
+
+    const loadPostComments = async () => {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
+        const data = await response.json();
+        setPostComments(data);
+    };
     return (
         <>
-        Comments
-        {/* Loop through comment data and display here */}
+            Comments
+            {Object.values(comments.map((comment, i) => (
+                <div key={i}>
+                    <div className="comment">
+                        <strong>{comment.name.charAt(0).toUpperCase() + comment.name.slice(1)}</strong>
+                        <br />
+                        <small>{comment.email}</small>
+                        <br />
+                        {comment.body}
+                    </div>
+                </div>
+            )))}
         </>
     )
 };
